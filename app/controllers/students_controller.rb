381d2fc570e785f68d2930ccab6f17e577173a29
Sharnell Grant <sharnellgrant@gmail.com>
2020-07-11 20:48:50 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,9 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+   
+    @students = Student.search(params[:query])
+    render 'index'
   end
 
   def student_params
