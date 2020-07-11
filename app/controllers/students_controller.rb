b23e0528f6c1cd21386099b74918f86a2f71dc02
Sharnell Grant <sharnellgrant@gmail.com>
2020-07-11 18:58:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -23,6 +23,7 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
     @students = Student.search(params[:query])
+    render 'index'
   end
 
   def student_params
